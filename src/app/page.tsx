0131c5ce291d1685614aa5f603230ec060a3d158
bradenslsb <bbir1199@gmail.com>
2024-06-22 08:48:20 -0600

    This is EOD commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="Main">
-      <h2>This is a header</h2>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      
     </main>
   );
 }
