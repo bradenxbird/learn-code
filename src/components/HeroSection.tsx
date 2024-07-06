e56af885c414340ca837fcc3396e45fd793a1469
bradenslsb <bbir1199@gmail.com>
2024-07-06 17:59:30 -0600

    this is a dumb commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,8 @@ const HeroSection = () => {
     return(
         <section>
             <h1 className="text-white">Hey Sexy</h1>
-            <h2>Can't believe it</h2>
+            <h2>Can't believe it</h2> 
+            <p>This is rediculous I need to code</p>
         </section>
     )
 }
