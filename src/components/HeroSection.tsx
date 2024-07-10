3ee9df27da76a2a118ac5babcfee27af99cb3b61
bradenslsb <bbir1199@gmail.com>
2024-07-10 17:08:55 -0600

    totally slacking over here

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         <section>
             <h1 className="text-white">Hey Sexy</h1>
             <h2>Can't believe it</h2> 
-            <p>This is rediculous I need to code</p>
+            <p>This is rediculous </p>
         </section>
     )
 }
