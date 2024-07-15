c271743c8847e8fd7bf498f009584830cf1ebe65
bradenslsb <bbir1199@gmail.com>
2024-07-14 19:38:27 -0600

    Another One -DJ Kahlid

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         <section>
             <h1 className="text-white">Hey Sexy</h1>
             <h2>Can't believe it</h2> 
-            <p>I need to follow along with this tutorial so that I can start building on my own. </p>
+            <p>What am I doing here </p>
         </section>
     )
 }
