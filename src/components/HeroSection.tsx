3f565b220a8a24d7a57a9ed9adfee9eecb4d068d
bradenslsb <bbir1199@gmail.com>
2024-07-24 17:54:59 -0600

    This is another commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         <section>
             <h1 className="text-white">Hey Sexy</h1>
             <h2>Can't believe it</h2> 
-            <p>What am I doing here, </p>
+            <p>What am I doing here, I don't think I have actually coded in a while </p>
         </section>
     )
 }
