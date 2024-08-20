9bdd64c25af61a643227692303aed861f01c2b9f
bradenslsb <bbir1199@gmail.com>
2024-08-20 10:54:58 -0600

    this commmit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return(
         <section>
             <h1 className="text-white">Hey Sexy</h1>
-            <h2>Can't believe it, i can't another weekend without coding, Yes</h2> 
+            <h2>Can't believe it, i can't another weekend without coding Yes</h2> 
         </section>
     )
 }
